a701c2b1bf3982ec98e585640a5ce1c505e4b700
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-02-18 00:27:52 +0530

    commit from rohan2

--- client/src/components/MentorDashboard/MentorDashboard.jsx
+++ client/src/components/MentorDashboard/MentorDashboard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const MentorDashboard = () => {
   return (
     <main className="w-full h-full flex flex-col gap-10">
@@ -8,10 +7,19 @@ const MentorDashboard = () => {
           Mentor Dashboard
         </div>
       </div>
-        <div className="min-h-[100vh] w-full flex flex-col gap-7">
-            <div className="h-[100px] w-full bg-slate-300"></div>
-            <div className="h-[100px] w-full bg-slate-300"></div>
+      <div className="min-h-[100vh] w-full flex flex-col gap-7">
+        <div className="h-[100px] w-full bg-slate-300">
+          <h1>John Dae</h1>
+          <div>
+            <h1>Need mentorship in:</h1>
+            <p>Interview preparation</p>
+            <button className="text-green">
+              <Check />
+            </button>
+          </div>
         </div>
+        <div className="h-[100px] w-full bg-slate-300"></div>
+      </div>
     </main>
   );
 };
