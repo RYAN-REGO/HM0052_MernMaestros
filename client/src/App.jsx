981737902484d8e6f27fd159e84be864990800d3
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-02-17 16:36:34 +0530

    first commit from rohan

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,40 @@
-import React, { useState } from 'react'
-import '@mantine/core/styles.css';
-import { Burger, MantineProvider } from '@mantine/core';
-import { Sidebar } from './components/Sidebar/Sidebar';
-import { useDisclosure } from '@mantine/hooks';
-
+import React, { useState } from "react";
+import "@mantine/core/styles.css";
+import { Burger, MantineProvider } from "@mantine/core";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { useDisclosure } from "@mantine/hooks";
 
 const App = () => {
   const [opened, { toggle }] = useDisclosure();
-  console.log(toggle,opened)
+  console.log(toggle, opened);
 
   return (
     <MantineProvider>
-      <div className='md:hidden'>
-        <Burger opened={opened} onClick={toggle} className='p-4' aria-label="Toggle navigation" />
-        {opened===true && <div className='absolute z-50 bg-white top-0'><Sidebar toggle={toggle}/></div>}
+      <div className="md:hidden">
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          className="p-4"
+          aria-label="Toggle navigation"
+        />
+        {opened === true && (
+          <div className="absolute z-50 bg-white top-0">
+            <Sidebar toggle={toggle} />
+          </div>
+        )}
       </div>
-      <div className='flex flex-row'>
-        <div className='hidden md:flex flex-row'>
-         <Sidebar toggle={toggle}/>
+      <div className="flex flex-row">
+        <div className="hidden md:flex flex-row">
+          <Sidebar toggle={toggle} />
         </div>
         {/* all routes goes here */}
-        <div className='p-4'>
-          sbdbfhdf jajfahjhfs afjasfjabfsaf sajfjasfuagfuiuafas fauhsfaubfsuaif
+        <div className="p-4">
+          I am good I am good I am good I am good I am good I am good I am good
+          I am good I am good I am good I am good I am good
         </div>
       </div>
     </MantineProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
